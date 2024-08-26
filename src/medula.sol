769c1bffab937d8e4f196248d218281896347bb1
John Fáwọlé <johnfawole18@gmail.com>
2024-08-27 00:58:32 +0200

    adding functions

--- src/medula.sol
+++ src/medula.sol
@@ -7,6 +7,10 @@
     address private payable audience;
     address private payable owner;
 
+    error OnlyForCreators();
+    error OnlyForAudience();
+    error OnlyTheOwner();
+
     enum ProductType {
         video,
         code,
@@ -19,6 +23,7 @@
         string creator;
         string name; 
         string description;
+        bool live;
     }
 
     mapping (address => uint) private creatorBalance;
@@ -26,29 +31,98 @@
     mapping (address => uint) private ownerMapping;
     mapping (uint256 => Product) public productMap;
 
-    uint private productId;
+    uint private productAdd;
+
+    constructor() public {
+        owner = msg.sender;
+    }
 
     function addProduct (
+        if(msg.sender = creator) {
+            revert OnlyForCreators();
+        }
         ProductType _type,
         string memory _creator,
         string memory _name,
         string memory _description
     ) public {
 
-         productMap[productId] = Product({
+         productMap[productAdd] = Product({
             type: _type;
             creator: _creator;
             name: _name;
-            description: _description
+            description: _description,
+            live: true
          })
+
+         productAdd++;
+         
+         creatorBalance[msg.sender].push(productAdd);
+
     }
 
-    function buyProduct() public {
+    function getAllProducts() public view returns (uint256[] memory) {
+        if(msg.sender = audience) {
+            revert OnlyForAudience();
+        }
+        uint256[] memory everyProducts = new uint256[](productAdd);
+
+        for (uint256 i = 1; i <= productAdd; i++) {
+            everyProducts[i - 1] = i
+        }
+
+         return everyProducts;
+    }
+
+    function getAProductParticulars(uint256 _itemId) public view returns (
+        if(msg.sender = audience) {
+            revert OnlyForAudience();
+        }
+        ProductType _type,
+        string memory _creator,
+        string memory _name,
+        string memory _description
+    ) {
+        Product memory item = productMap[_itemId];
+    }
+
+    function buyProduct(uint256 _itemId) public {
+        if(msg.sender = audience) {
+            revert OnlyForAudience();
+        }
+
+        Product memory item = productMap[_itemId];
+
+        (bool success, ) = item.creator.call{value: msg.value}("");
+        require(success, "error - failed transaction");
         
     }
 
-    function removeProduct() public {
+    function removeProduct(uint256 _itemId) public {
+        if(msg.sender = owner) {
+            revert OnlyTheOwner();
+        }
+        Product memory item = productMap[_itemId];
 
+        item.live = false;
+    }
+
+    function removeCreator(address _creator) public {
+        if(msg.sender = owner) {
+            revert OnlyTheOwner();
+        }
+        uint256[] storage creatorItemId = creatorMap[_creator];
+
+        delete creatorItemId[_creator];
+    }
+
+    function removeAudience(address _audience) public {
+        if(msg.sender = owner) {
+            revert OnlyTheOwner();
+        }
+        uint256[] storage audienceItemId = audienceMap[_audience];
+
+        delete audienceItemId[_audience];
     }
 
 
